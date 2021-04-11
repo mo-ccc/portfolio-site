50607c1c503eadb493736f5475feafdf7ae9a2b7
mo <css012002@coderacademy.edu.au>
2021-04-11 19:43:18 +1000

    added client side routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Redirect, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/">
+          <h1>home</h1>
+        </Route>
+        <Route exact path="/about">
+          <h1>about</h1>
+        </Route>
+        <Route exact path="/projects">
+          <h1>projects</h1>
+        </Route>
+        <Route exact path="/404">
+          <h1>404</h1>
+        </Route>
+        <Route>
+          <Redirect to="/404"/>
+        </Route>
+      </Switch>
     </div>
   );
 }
